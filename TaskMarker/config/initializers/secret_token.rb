9f2e0a24351858fee154760e631a12205f88aed0
Shrikant Adhikarla <sadhikar@andrew.cmu.edu>
2011-09-17 01:15:36 -0500

    Initial

---
+++ TaskMarker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskMarker::Application.config.secret_token = 'c78bad0634ad61d86cb1defba7a9b4d7caea8f4d048c97a13144b920b5e56b7521ee1890fc9c50fad664d09397d407e9d6ff3222834212f4d4a229520d172578'
